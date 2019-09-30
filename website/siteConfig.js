a90c6bfd13464cde2cee083dea6c854f4a86099e
AmalieDue <amalieduejensen@hotmail.com>
2019-09-30 13:16:39 +0200

    Section with API added

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc1', label: 'Get Started'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
