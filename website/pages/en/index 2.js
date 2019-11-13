51b84336fa6218851d9d10db3aec40247a24638d
AmalieDue <amalieduejensen@hotmail.com>
2019-11-13 11:08:53 +0100

    new build

---
+++ website/pages/en/index 2.js
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Logo = props => (
+      <div className="projectLogo">
+        <img src={props.img_src} alt="Project Logo" />
+      </div>
+    );
+
+    const ProjectTitle = () => (
+      <h2 className="projectTitle inverse">
+        {siteConfig.title}
+        <small>{siteConfig.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = props => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Button = props => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
+
+    return (
+      <SplashContainer>
+        <div className="inner">
+          <ProjectTitle siteConfig={siteConfig} />
+          <div className="buttons">
+            <a className="button" href="/docusaurus/docs/getstarted">GET STARTED</a>
+          </div>
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
+    const Block = props => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const Features = () => (
+      <Block layout="fourColumn">
+        {[
+          {
+            content: 'See the [Libsodium docs](https://libsodium.gitbook.io/doc/) for more information',
+            image: `${baseUrl}img/icons8-checkmark-96.png`,
+            imageAlign: 'top',
+            title: 'Libsodium',
+          },
+          {
+            content: 'See sodium-native on GitHub [here](https://github.com/sodium-friends/sodium-native)',
+            image: `${baseUrl}img/GitHub-Mark-120px-plus.png`,
+            imageAlign: 'top',
+            title: 'GitHub'
+          },
+          {
+            content: 'Learn cryptographic engineering through a set of exercises [here](https://github.com/sodium-friends/learntocrypto)',
+            image: `${baseUrl}img/icons8-checkmark-96.png`,
+            imageAlign: 'top',
+            title: 'Crypto workshop',
+          },
+          {
+            content: 'Learn more about scaling, distributed systems, and crypto at [Hyperdivision](https://hyperdivision.dk/)',
+            image: `${baseUrl}img/hyperdivision-logo.svg`,
+            imageAlign: 'top',
+            title: 'Hyperdivision',
+          }
+        ]}
+      </Block>
+    );
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+          <Features />
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
+
+
+
