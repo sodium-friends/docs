d9c8407b994eb1c4594d1609fc56313ba3f57531
Mathias Buus <mathiasbuus@gmail.com>
2019-11-20 11:56:21 +0100

    updated config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'sodium-native', // Title for your website.
   tagline: 'Low level bindings for Libsodium',
-  url: 'Hyperdivision.github.io', // Your website URL
-  baseUrl: '/docusaurus/', // Base URL for your project */
+  url: 'sodium-friends.github.io', // Your website URL
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docusaurus',
+  projectName: 'docs',
   organizationName: 'Hyperdivision',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
